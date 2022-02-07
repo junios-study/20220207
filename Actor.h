7b4c77276ef8a33878a1e89679b18102220c9c01
wontae <83wontae@gmail.com>
2022-02-07 11:00:41 +0900

    inline 접근자 만들기

--- Actor.h
+++ Actor.h
@@ -4,5 +4,20 @@ class Actor
 public:
 	Actor();
 	virtual ~Actor();
+
+	virtual void Tick();
+	virtual void Render();
+	virtual void BeginPlay();
+
+	inline virtual void SetActorLocation(int NewX, int NewY);
+	inline virtual void SetShape(char NewShape);
+	inline virtual char GetShape() { return Shape; }
+	inline virtual int GetX() { return X; }
+	inline virtual int GetY() { return Y; }
+
+protected:
+	int X;
+	int Y;
+	char Shape;
 };
 
